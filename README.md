9b9f63d5119c1ff416937665faaed8e3d851819b
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-12 09:21:56 +0530

    body data fetched

--- README.md
+++ README.md
@@ -36,3 +36,9 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+const data = [ {id="1" children:[{text:"this is first text"}]},
+{id="2" children:[{text:"this is second text"}]},
+{id="3" children:[{text:"this is third text"}]},
+{id="4" children:[{text:"this is fourth text"}]},
+]
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,50 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter, Ultra } from "next/font/google";
 import { createClient } from "next-sanity";
 const inter = Inter({ subsets: ["latin"] });
+import PortableText from "react-portable-text";
 
-export default function Home({ blogs }) {
-  console.log(blogs);
+export default function Home({ posts }) {
+  console.log(posts[1]);
 
-  return <div>Home page!!</div>;
+  return (
+    <div>
+      <h1>home</h1>
+      <h1>Title: {posts[1].title}</h1>
+      <h2>data: {posts[1]._createdAt}</h2>
+      {posts[1].body.map((bodyy) => {
+        return (
+          <>
+            <ul key={bodyy?._key}>
+              <li>{bodyy?.children[0].text}</li>
+            </ul>
+          </>
+        );
+      })}
+      {/* <Image
+        src={posts[1].mainImage.asset._ref}
+        alt="Post-image"
+        height={200}
+        width={300}
+      /> */}
+      {/* <div>
+        <PortableText
+          // Pass in block content straight from Sanity.io
+          content={posts[0].content}
+          projectId="q4737knk"
+          dataset="production"
+          // Optionally override marks, decorators, blocks, etc. in a flat
+          // structure without doing any gymnastics
+          serializers={{
+            h1: (props) => <h1 style={{ color: "red" }} {...props} />,
+            li: ({ children }) => (
+              <li className="special-list-item">{children}</li>
+            ),
+          }}
+        />
+      </div> */}
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
@@ -16,10 +54,10 @@ export async function getServerSideProps(context) {
     useCdn: false,
   });
   const query = `*[_type=="post"]`;
-  const blogs = await client.fetch(query);
+  const posts = await client.fetch(query);
   return {
     props: {
-      blogs,
+      posts,
     },
   };
 }
